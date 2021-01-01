cfbe4a2b9fdc5f25440a8ca9e800c37f57c3f2a4
Ilyas D Jayanto <ell@petaniweb.com>
2021-01-01 17:30:07 +0700

    feat: support upload image when create event

--- src/interfaces/http/routes/event.ts
+++ src/interfaces/http/routes/event.ts
@@ -1,10 +1,30 @@
 import express, { Request, Response } from 'express';
+import multer from 'multer';
+import * as path from 'path';
 import validator from './validation/event';
 import eventBiz from '../../../domain/event/business';
 import { log } from '../../../helper/logger';
 
 export default () => {
   const router = express.Router();
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'public/images');
+    },
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + path.extname(file.originalname));
+    },
+  });
+
+  const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+      cb(null, true);
+    } else {
+      cb(null, false);
+    }
+  };
+
+  const upload = multer({ storage, fileFilter });
 
   router
     .route('/')
@@ -20,13 +40,17 @@ export default () => {
   router
     .route('/')
     .post(
+      upload.single('image'),
       validator.createEvent,
       async (req: Request, res: Response) => {
         const { tid } = res.locals;
-
         log('info', 'create-event-input', { body: req.body, tid });
 
-        const result = await eventBiz.createEvent(req.body);
+        const targetPath = `public/images/${req.file.originalname}`;
+
+        const params = { ...req.body, image: targetPath };
+        const result = await eventBiz.createEvent(params);
+
         const statusCode = (!result.status) ? 500 : 200;
 
         return res.status(statusCode).json(result);
