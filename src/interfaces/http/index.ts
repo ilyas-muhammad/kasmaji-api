2002b4d1c20947711dd4fea32f2f9edc727da85a
Ilyas D Jayanto <ell@petaniweb.com>
2021-01-16 10:57:34 +0700

    add compression and helmet

--- src/interfaces/http/index.ts
+++ src/interfaces/http/index.ts
@@ -3,11 +3,16 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import * as uuid from 'uuid';
 import bodyParser from 'body-parser';
+import compression from 'compression';
+import helmet from 'helmet';
 import { log } from '../../helper/logger';
 import routes from './routes';
 
 dotenv.config();
 const app = express();
+app.use(compression());
+app.use(helmet());
+
 const tid = uuid.v4();
 
 app.use((req, res, next) => {
