80af17db4c4bd61b7fecfd39258ba3707672d272
Ilyas D Jayanto <ell@petaniweb.com>
2021-01-16 10:24:25 +0700

    refactor routes

--- src/interfaces/http/routes/index.ts
+++ src/interfaces/http/routes/index.ts
@@ -5,12 +5,12 @@ import { Request, Response, Express } from 'express';
 export default (app: Express) => {
   const routes = requireDir('./');
 
-  app.get('/', (_: Request, res: Response) => res.send('Kasmaji API'));
-
   // Initialize all routes
   Object.keys(routes).forEach((routeName) => {
     const endpoint = changeCase.paramCase(routeName);
 
     app.use(`/${endpoint}`, routes[routeName].default());
   });
+
+  app.get('/', (_: Request, res: Response) => res.send('Kasmaji API'));
 };
